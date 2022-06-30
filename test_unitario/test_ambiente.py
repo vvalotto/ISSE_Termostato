c77357d1b68fa1f2378c83ebcc0302474bf678cb
Victor Valotto <vvalotto@gmail.com>
2022-06-30 18:02:04 -0300

    Test Unitario - Ejemplo 2 - Preparación de la corrida de tests

--- test_unitario/test_ambiente.py
+++ test_unitario/test_ambiente.py
@@ -4,14 +4,17 @@ from entidades.ambiente import *
 
 class TestAmbiente(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.__ambiente = Ambiente()
+
     def test_temperatura_ambiente(self):
-        Ambiente.temperatura_ambiente = 5
-        self.assertEqual(Ambiente.temperatura_ambiente, 5)
+        self.__ambiente.temperatura_ambiente = 20
+        self.assertEqual(self.__ambiente.temperatura_ambiente, 20)
 
     def test_temperatura_deseada(self):
-        Ambiente.temperatura_deseada = 5
-        self.assertAlmostEqual(Ambiente.temperatura_deseada, 5)
+        self.__ambiente.temperatura_deseada = 24
+        self.assertAlmostEqual(self.__ambiente.temperatura_deseada, 24)
 
     def test_temperatura_a_mostrar(self):
-        Ambiente.temperatura_a_mostrar = "ambiente"
-        self.assertAlmostEqual(Ambiente.temperatura_a_mostrar, "ambiente")
+        self.__ambiente.temperatura_a_mostrar = "ambiente"
+        self.assertAlmostEqual(self.__ambiente.temperatura_a_mostrar, "ambiente")
